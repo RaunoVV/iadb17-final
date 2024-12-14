e238e0172523404acfe32f83b659fe5a021003dd
RaunoVV <rauno123@gmail.com>
2024-12-14 15:12:00 +0200

    Use mocking iwth USE_MOCk env var
    Signed-off-by: RaunoVV <rauno123@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-    plugins: [react()],
-    build: {
-        target: "es2022", // or 'esnext'
-    },
+// export default defineConfig(({mode}) => {
+//     plugins: [react()],
+//     optimizeDeps: {
+//         exclude: [], // Exclude react-native from Vite's dependency optimization
+//     },
+//     build: {
+//         target: "es2022", // or 'esnext'
+//         rollupOptions: {
+//             external: [], // Exclude react-native from the bundle
+//         },
+//     },
+// });
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        define: {
+            "process.env.USE_MOCK": JSON.stringify(env.USE_MOCK),
+        },
+        plugins: [react()],
+        build: {
+            target: "es2022", // or 'esnext'
+            rollupOptions: {
+                external: [], // Exclude react-native from the bundle
+            },
+        },
+    };
 });
