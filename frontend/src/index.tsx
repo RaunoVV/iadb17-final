146234b7693646ce51fb9157688bf466435171cc
RaunoVV <rauno123@gmail.com>
2024-12-14 14:02:51 +0200

    Use mocking iwth USE_MOCk env var
    Signed-off-by: RaunoVV <rauno123@gmail.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,18 +7,18 @@ import { worker } from "./mocks/server";
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
-if (process.env.NODE_ENV === "development2") {
-	await worker.start().then((r) =>
-		root.render(
-			<React.StrictMode>
-				<App />
-			</React.StrictMode>,
-		),
-	);
+if (process.env.USE_MOCK === "yes") {
+    await worker.start().then(() =>
+        root.render(
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>,
+        ),
+    );
 } else {
-	root.render(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
-	);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    );
 }
